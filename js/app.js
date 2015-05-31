9fa2b39be1b29deadb2f049219439b198a9fdd29
Chris Archibald <carchi8py@gmail.com>
2015-05-30 17:40:37 -0700

    Getting a random bug to appear and move on the screen

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    this.speed = Math.floor(Math.random()*(1000)+100);
+    //X and Y are the location of the player
+    this.x = x;
+    this.y = y;
 }
 
 // Update the enemy's position, required method for game
@@ -14,6 +19,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+
+    if (this.x > 505) {
+        this.x = -101
+    } else {
+        this.x += dt * this.speed
+    }
 }
 
 // Draw the enemy on the screen, required method for game
@@ -25,10 +36,55 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(x, y) {
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
+}
+
+Player.prototype.update = function(dt) {
+
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function(key){
+    switch(key) {
+        case 'up':
+            if (this.y > 0) {
+                this.y -= 83;
+            }
+            break;
+        case 'down':
+            if (this.y <404) {
+                this.y += 83;
+            }
+            break;
+        case 'left':
+            if (this.x > 0) {
+                this.x -= 101;
+            }
+            break;
+        case 'right':
+            if (this.x < 404) {
+                this.x += 101;
+            }
+            break;
+    }
+}
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+var en = new Enemy(0, 140);
+
+var allEnemies = [en];
+
+var player = new Player(202,404)
 
 
 
